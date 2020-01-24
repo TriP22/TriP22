aae49ad8f755badeb19926800754ee1d180b6448
TriP22 <60031832+TriP22@users.noreply.github.com>
2020-01-25 00:04:09 +0530

    Slider update

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,4 +9,39 @@ $(document).ready(function(){
     nextArrow:'<button type="button" class="btn slick-next shadow-sm" style="background-color:#fff;height:40px;width:40px;border-radius:50%;"></button>',
     prevArrow:'<button type="button" class="btn slick-prev">Previous</button>',
     });
-    });
+    $('.responsive-card-1').slick({
+        dots:false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
+});
