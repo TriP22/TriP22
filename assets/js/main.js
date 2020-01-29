f59de172f65f74c87b4ae7dcd61f0b4355c32502
TriP22 <60031832+TriP22@users.noreply.github.com>
2020-01-29 11:39:16 +0530

    main update7

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     arrows: true,
     infinite:false,
     nextArrow:'<button type="button" class="btn slick-next shadow-sm th-btn-slider"></button>',
-    prevArrow:'<button type="button" class="btn slick-prev shadow-sm th-btn-slider" ></button>',
+    prevArrow:'<button type="button" class="btn slick-prev shadow-sm th-btn-slider"></button>',
     responsive: [
       {
         breakpoint: 1024,
