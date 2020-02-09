edc8a009ba6e7b728276ab649287ba63392185e6
AMIT ANAND <amitanand.kvj.2@gmail.com>
2020-02-09 17:24:41 +0530

    main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,6 +164,47 @@ $(document).ready(function(){
         // instead of a settings object
       ]
     });
+    $('.slider-popular-activity').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: false,
+        autoplaySpeed: 4000,
+        dots:false,
+        arrows: true,
+        infinite:false,
+        nextArrow:$(".nextp1"),
+        prevArrow:$(".prevp1"),
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: false,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
   $('.responsive-card-1').slick({
     dots:false,
     infinite: false,
